e9f02de7b5b5a0ad6e635a2ef55d1f55b87137b4
Mathieu Huot <mathieu.huot@gmail.com>
2022-03-30 15:48:44 -0400

    Got further in the design.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,6 @@
 module.exports = {
   genericTitle: "Eleventy-esbuild-swc",
   description: "An example of integration of esbuild and swc with Eleventy, the static site generator.",
-  eleventyEnv: process.env.ELEVENTY_ENV
+  eleventyEnv: process.env.ELEVENTY_ENV,
+  canonical: "#"
 }
