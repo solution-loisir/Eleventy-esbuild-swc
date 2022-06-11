28074740e129f3fcc70b13839587c105a0a6e01f
Mathieu Huot <mathieu.huot@gmail.com>
2022-06-10 21:27:23 -0400

    Refactored the lot.

--- _11ty/shortcodes/code-example.js
+++ _11ty/shortcodes/code-example.js
@@ -17,7 +17,7 @@ module.exports = ({ css, html, js }) => {
   :
   "";
 
-  return `<section class="[ code-example ] [ wrapper ]" data-min-width="full">
+  return `<section class="[ code-example ] [ wrapper ]">
   <h1>CODE EXAMPLE</h1>
 
   <details-utils animate>
